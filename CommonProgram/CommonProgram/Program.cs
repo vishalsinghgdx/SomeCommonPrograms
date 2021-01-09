96e9a1d66cb45dbd9f06617f09f002d8669cca01
vishal <vishalkiet09@gmail.com>
2021-01-09 18:29:59 +0530

    Smallest positive number that is evenly divisible by all of the numbers from 1 to 10

---
+++ CommonProgram/CommonProgram/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CommonProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmallestDivisible();
+            Console.ReadLine();
+        }
+
+        static void SmallestDivisible()
+        {
+            // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            int max = 20;
+            int SN = max;
+            int div = 2;
+
+            while (div < max)
+            {
+                if (SN % div == 0)
+                {
+                    div += 1;
+                    if (div == max)
+                    {
+                        Console.WriteLine(SN);
+                    }
+                }
+                else
+                {
+                    SN = SN + max;
+                    div = 2;
+                }
+            }
+        }
+    }
+}
