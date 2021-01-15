cce008eb8ad71fe9d41eac9bfebb793c8bbce154
vishal <vishalkiet09@gmail.com>
2021-01-15 15:39:11 +0530

    Implementing custom array methods

---
+++ CommonProgram/CommonProgram/CustomArray.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+/// <summary>
+/// Implementing custom array methods
+/// </summary>
+namespace CommonProgram
+{
+    class CustomArray
+    {
+        int[] numbers;
+        int count;
+
+        public CustomArray(int len)
+        {
+            numbers = new int[len];
+        }
+
+        public void Insert(int number)
+        {
+            if (numbers.Length == count)
+            {
+                // create new array
+                int[] newArray = new int[count * 2];
+                // copy old array items to new array
+                for (int i = 0; i < count; i++)
+                {
+                    newArray[i] = numbers[i];
+                }
+                // assing new array to old array
+                numbers = newArray;
+            }
+            numbers[count++] = number;
+        }
+
+        public void RemoveAt(int index) 
+        {
+            if (index > count - 1 || index < 0)
+            {
+                Console.WriteLine("Index is out of bound");
+                return;
+            }
+            for (int i = index; i < count - 1; i++)
+            {
+                numbers[i] = numbers[i + 1];
+            }
+            count--;
+        }
+
+        public void ItemAtIndex(int index)
+        {
+            if(index < 0 || index >= count)
+            {
+                Console.WriteLine("Searched index out of bound");
+                return;
+            }
+            Console.WriteLine(numbers[index]);
+        }
+
+        public int IndexOf(int item)
+        {
+            for(int i = 0; i < count; i++)
+            {
+                if(numbers[i] == item)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
--- CommonProgram/CommonProgram/Program.cs
+++ CommonProgram/CommonProgram/Program.cs
@@ -15,7 +15,18 @@ namespace CommonProgram
             //ReverseStringArray.ReverseBySwapping();
 
             //ReverseString.Reverse();
-            ReverseString.Reverse2();
+            //ReverseString.Reverse2();
+
+            CustomArray array = new CustomArray(3);
+            array.Insert(10);
+            array.Insert(15);
+            array.Insert(20);
+            array.Insert(25);
+            array.Insert(30);
+            array.ItemAtIndex(4);
+            Console.WriteLine(array.IndexOf(50));
+            //array.RemoveAt(1);
+            array.Print();
 
             Console.ReadLine();
         }
