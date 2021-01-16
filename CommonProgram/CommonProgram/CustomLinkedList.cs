756a51bf8f928798f727d3f80d10fdec2a67010f
vishal <vishalkiet09@gmail.com>
2021-01-16 16:39:29 +0530

    Implementing custom linked list

---
+++ CommonProgram/CommonProgram/CustomLinkedList.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace CommonProgram
+{
+    class CustomLinkedList
+    {
+        public class Node
+        {
+            public int value;
+            public Node next;
+        }
+
+        Node first;
+        Node last;
+
+        public void AddLast(int item)
+        {
+            var node = new Node();
+            node.value = item;
+
+            if (isEmpty())
+            {
+                first = last = node;
+            }
+            else
+            {
+                last.next = node;
+                last = node;
+            }
+        }
+
+        public void AddFirst(int item)
+        {
+            var node = new Node();
+            node.value = item;
+            if (isEmpty())
+            {
+                first = last = node;
+            }
+            else
+            {
+                node.next = first;
+                first = node;
+            }
+        }
+
+        public int IndexOf(int item)
+        {
+            var current = first;
+            int index = 0;
+            while(current != null)
+            {
+                if(current.value == item)
+                {
+                    return index;
+                }
+                current = current.next;
+                index++;
+            }
+            return -1;
+        }
+
+        public bool Contains(int item)
+        {
+            if (IndexOf(item) != -1)
+                return true;
+
+            return false;
+        }
+
+        public void RemoveFirst()
+        {
+            if (isEmpty())
+                throw new InvalidOperationException();
+
+            if (first == last)
+            {
+                first = last = null;
+                return;
+            }
+                
+            var second = first.next;
+            first.next = null;
+            first = second;
+        }
+
+        public void RemoveLast()
+        {
+            if (isEmpty())
+                throw new InvalidOperationException();
+
+            if (first == last)
+            {
+                first = last = null;
+                return;
+            }
+                
+            var current = first;
+            while(current != null)
+            {
+                if (current.next == last)
+                    break;
+
+                current = current.next;
+
+            }
+
+            current.next = null;
+            last = current;
+        }
+
+        public void Remove(int item)
+        {
+
+        }
+             
+
+        private bool isEmpty()
+        {
+            return first == null;
+        }
+    }
+}
--- CommonProgram/CommonProgram/Program.cs
+++ CommonProgram/CommonProgram/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace CommonProgram
 {
@@ -17,16 +19,39 @@ namespace CommonProgram
             //ReverseString.Reverse();
             //ReverseString.Reverse2();
 
-            CustomArray array = new CustomArray(3);
-            array.Insert(10);
-            array.Insert(15);
-            array.Insert(20);
-            array.Insert(25);
-            array.Insert(30);
-            array.ItemAtIndex(4);
-            Console.WriteLine(array.IndexOf(50));
+            //CustomArray array = new CustomArray(3);
+            //array.Insert(10);
+            //array.Insert(15);
+            //array.Insert(20);
+            //array.Insert(25);
+            //array.Insert(30);
+            //array.ItemAtIndex(4);
+            //Console.WriteLine(array.IndexOf(50));
             //array.RemoveAt(1);
-            array.Print();
+            // array.Print();
+
+            //ArrayList list = new ArrayList();
+            //list.Add(10);
+            //list.Add(20);
+            //list.Add(30);
+            //foreach(var l in list)
+            //{
+            //    Console.WriteLine(l);
+            //}
+
+            //LinkedList<int> linked = new LinkedList<int>();
+
+            CustomLinkedList linkedList = new CustomLinkedList();
+            linkedList.AddLast(10);
+            linkedList.AddLast(20);
+            linkedList.AddLast(30);
+            linkedList.AddFirst(5);
+            linkedList.AddLast(25);
+            //Console.WriteLine(linkedList.IndexOf(25));
+            //Console.WriteLine(linkedList.Contains(50));
+            linkedList.RemoveFirst();
+            linkedList.RemoveFirst();
+            linkedList.RemoveLast();
 
             Console.ReadLine();
         }
